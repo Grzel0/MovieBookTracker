1406f2be2c026444574835dd0dddbca6ce207a59
Grzegorz <grzegorz.gejlo@cyfroweszkoly.pl>
2024-11-30 16:11:27 +0100

    Dodanie onCreateViewHolder oraz onBindViewHolder w ListaAdapter

--- app/src/main/java/com/example/moviebooktracker/ListaAdapter.kt
+++ app/src/main/java/com/example/moviebooktracker/ListaAdapter.kt
@@ -1,6 +1,8 @@
 package com.example.moviebooktracker
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.CheckBox
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
@@ -17,4 +19,23 @@ class ListaAdapter(private val lista: List<ElementListy>) :
         val obejrzane: CheckBox = view.findViewById(R.id.obejrzaneCheckbox)
     }
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.element_listy, parent, false)
+        return ViewHolder(view)
+    }
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val element = lista[position]
+        holder.tytul.text = element.tytul
+        holder.gatunek.text = element.gatunek
+        holder.recenzja.text = element.recenzja
+        holder.ocena.text = "Ocena: ${element.ocena}"
+        holder.typ.text = element.typ
+        holder.obejrzane.isChecked = element.obejrzane
+
+        holder.obejrzane.setOnCheckedChangeListener { _, isChecked ->
+            element.obejrzane = isChecked
+        }
+    }
+    override fun getItemCount(): Int = lista.size
 }
