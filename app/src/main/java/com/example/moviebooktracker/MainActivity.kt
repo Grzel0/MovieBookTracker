28d2dd2fdba452d82ed1a6613945ecaf529a62e6
Grzegorz <grzegorz.gejlo@cyfroweszkoly.pl>
2024-11-30 16:25:22 +0100

    dodanie obsługi formularza w MainActivity

--- app/src/main/java/com/example/moviebooktracker/MainActivity.kt
+++ app/src/main/java/com/example/moviebooktracker/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.example.moviebooktracker
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.SeekBar
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +22,27 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        lateinit var lista: MutableList<ElementListy>
+        lateinit var adapter: ListaAdapter
+        lista = mutableListOf()
+        adapter = ListaAdapter(lista)
+
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = adapter
+
+        val tytul = findViewById<EditText>(R.id.tytul)
+        val gatunek = findViewById<EditText>(R.id.gatunek)
+        val recenzja = findViewById<EditText>(R.id.recenzja)
+        val ocena = findViewById<SeekBar>(R.id.ocena)
+        val typGroup = findViewById<RadioGroup>(R.id.typ)
+        val dodajButton = findViewById<Button>(R.id.dodajButton)
+
+        dodajButton.setOnClickListener {
+            val wybranyTyp = findViewById<RadioButton>(
+                typGroup.checkedRadioButtonId
+            )?.text?.toString() ?: "Nieznany"
         }
     }
 }
